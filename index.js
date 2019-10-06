a206cee99d4c499a57cbc129b8d70e3aacb41aab
Kevin Karma <kevinkarma55@gmail.com>
2019-10-06 17:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ lowerCaseDrivers = (drivers) => {
   return drivers.map( (driver) => driver.toLowerCase())
 }
 
-nameToAttributes = (array) => {
+function nameToAttributes(array) {
   return array.map( (driver) => ({
     firstName: driver.split(" ")[0], 
     lastName: driver.split(" ")[1]
